389f6119b9fe9f969aac6b30213d2a34a72ece72
Tomasz Pewiński <pewniak747@gmail.com>
2013-01-14 20:22:01 +0100

    move stuff around, pretend to do work

--- src/ant_colony/main.cpp
+++ src/ant_colony/main.cpp
@@ -1,38 +1,4 @@
-#include<algorithm>
-#include<cstdio>
-#include<vector>
-#include<cmath>
-#include<cstring>
-#define INF 2000000000
-#define REP(i,n) for(int i = 0; i < (n); i++)
-#define FOR(i, a, b) for(int i = (a); i < (b); i++)
-#define FORD(i, a, b) for(int i = (a); i >= (b); i--)
-#define PI pair<int, int>
-#define ST first
-#define ND second
-#define CLR(a, b) memset(a, b, sizeof(a))
-#ifdef DEBUG
-  #define DBG printf
-#else
-  #define DBG
-#endif
-using namespace std;
-
-class Graph{
-  public:
-    int size;
-    vector< vector<int> > distance;
-
-    void init(int graphSize){
-      size = graphSize;
-      distance.resize(size);
-      REP(i, size) distance[i].resize(size);
-      return;
-    }
-
-    void addEdge(int x, int y, int length){ distance[x][y] = length;}
-
-};
+#include "../common.h"
 
 class CNGraph : public Graph{
   public:
@@ -60,22 +26,6 @@ class CNGraph : public Graph{
     int isVisited(int v){ return visited[v];}
 };
 
-
-// This function could also be part of graph class
-void readData(CNGraph &G){
-  int size, dist;
-  scanf("%d",&size);
-  G.init(size);
-
-  REP(i, G.size)
-    REP(j, G.size){
-      scanf("%d",&dist);
-      G.addEdge(i, j, dist);
-    }
-  return;
-}
-
-
 // TODO Refactor
 // This whole function smells and should be partially incorporated into CNGraph.
 int solve(CNGraph &G, vector<int> &path){
@@ -94,15 +44,6 @@ int solve(CNGraph &G, vector<int> &path){
 
   return sum;
 }
-
-void printResult(int result, vector<int> &path){
-  printf("%d\n",result);
-  REP(i, path.size()) printf("%d ",path[i]);
-  printf("\n");
-}
-
-
-
 
 int main(){
   CNGraph G;
---
+++ src/common.h
@@ -0,0 +1,57 @@
+#include<algorithm>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<cstring>
+
+#define INF 2000000000
+#define REP(i,n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define FORD(i, a, b) for(int i = (a); i >= (b); i--)
+#define PI pair<int, int>
+#define ST first
+#define ND second
+#define CLR(a, b) memset(a, b, sizeof(a))
+
+#ifdef DEBUG
+  #define DBG printf
+#else
+  #define DBG
+#endif
+
+using namespace std;
+
+class Graph{
+  public:
+    int size;
+    vector< vector<int> > distance;
+
+    virtual void init(int graphSize){
+      size = graphSize;
+      distance.resize(size);
+      REP(i, size) distance[i].resize(size);
+      return;
+    }
+
+    void addEdge(int x, int y, int length){ distance[x][y] = length;}
+
+};
+
+void readData(Graph &G){
+  int size, dist;
+  scanf("%d",&size);
+  G.init(size);
+
+  REP(i, G.size)
+    REP(j, G.size){
+      scanf("%d",&dist);
+      G.addEdge(i, j, dist);
+    }
+}
+
+void printResult(int result, vector<int> &path){
+  printf("%d\n",result);
+  REP(i, path.size()) printf("%d ",path[i]);
+  printf("\n");
+}
+
