3d02ddbfe6ef9624c9db45bef39d730ac2229316
Piotr Żurkowski <kareth92@gmail.com>
2013-01-15 19:09:34 +0100

    Ant colony first version, not working

--- src/ant_colony/main.cpp
+++ src/ant_colony/main.cpp
@@ -1,53 +1,144 @@
 #include "../common.h"
 
-class CNGraph : public Graph{
+const double BETA = 1;
+const double ALPHA = 0.5;
+const double BASIC_PHEROMONE = 0.0001;
+const double Q = 0.5;
+
+const int STEPS = 50;
+const int ANTS = 50;
+
+double rand01(){
+  return double(rand()) / double(RAND_MAX);
+}
+
+class AOGraph : public Graph{
   public:
-    vector<int> visited;
+    vector<vector<double> > pheromone;
 
     void init(int graphSize){
       Graph::init(graphSize);
-      visited.resize(size);
+      pheromone.resize(size);
+      REP(i, size)pheromone[i].resize(size);
     }
-    int closestNotVisited(int v){
 
-      // TODO This selection function needs to be changed
-      int closest = v;
-      REP(i, size){
-        if( i == v || isVisited(i)) continue;
-        if( closest == v) closest = i;
+    double score(int x, int y){
+      return pheromone[x][y] * pow(1.0/distance[x][y], BETA);
+    }
 
-        if( distance[v][i] < distance[v][closest])
-          closest = i;
+    void updateLocalTrial(int x, int y){
+      pheromone[x][y] = (1.0 - ALPHA)* pheromone[x][y] + ALPHA * BASIC_PHEROMONE;
+    }
+
+    void updateGlobally(vector<int> &path, int length){
+      REP(i, path.size()-1){
+        int x = path[i], y = path[i+1];
+        pheromone[x][y] = (1.0 - ALPHA) * pheromone[x][y] + ALPHA / double(length);
       }
-      return closest;
     }
-
-    void visit(int v){ visited[v] = 1;}
-    int isVisited(int v){ return visited[v];}
 };
 
-// TODO Refactor
-// This whole function smells and should be partially incorporated into CNGraph.
-int solve(CNGraph &G, vector<int> &path){
-  int startingVerticle = 0; // We can think of any way of improving that, becouse other starting points will give other results
+AOGraph G;
 
-  path.push_back(startingVerticle);
-  G.visit(path.back());
 
-  while(path.size() < G.size){
-    path.push_back(G.closestNotVisited(path.back()));
-    G.visit(path.back());
+class Ant{
+  vector<int> visited;
+  int distanceTravelled;
+
+
+  private:
+    bool isVisited(int v){
+      return find(visited.begin(), visited.end(), v) != visited.end();
+    }
+
+    int lastCity(){return visited.back();}
+
+    int moveToBest(){
+      int best = lastCity();
+      REP(i, G.size){
+        if ( !isVisited(i) ){
+          if( best == lastCity()) best = i;
+          if( G.score(lastCity(), best) < G.score(lastCity(), i))
+            best = i;
+        }
+      }
+      return best;
+    }
+
+    int moveRandomly(){
+      vector<double> scores;
+      double sum = 0;
+      sum = getScores(scores);
+      return getRandom(scores, sum);
+    }
+
+    int getRandom(vector<double> scores, double sum){
+      double r = rand01()*sum;
+      int i;
+      for(i = 0; i < scores.size() && r > 0; i++){
+        r -= G.score(lastCity(), scores[i]);
+      }
+      return i;
+    }
+
+    int getScores(vector<double> &scores){
+      int sum = 0.0;
+      REP(i, G.size){
+        if(!isVisited(i)){
+          int score = G.score(lastCity(), i);
+          scores.push_back(score);
+          sum += score;
+        }
+      }
+      return sum;
+    }
+
+  public:
+    Ant(){
+      visited.push_back(rand()%G.size);
+    }
+
+    void move(){
+      int next;
+      if (rand01() < Q) next = moveToBest();
+      else next = moveRandomly();
+
+      G.updateLocalTrial(lastCity(), next);
+    }
+
+    int getPath(){ return distanceTravelled;}
+    int getPath(vector<int> &v){ v = visited; return distanceTravelled;}
+};
+
+int solve(AOGraph &G, vector<int> &path){
+  vector<int> bestPath;
+  int bestPathLength = 1000000000;
+  REP(i, STEPS){
+    vector<Ant> ants;
+    REP(j, ANTS){
+      Ant newAnt;
+      ants.push_back(newAnt);
+    }
+
+    REP(j, G.size-1)
+      REP(k, ANTS)
+        ants[k].move();
+
+    int shortestAnt = 0;
+    int shortestPathLength;
+    vector<int> shortestPath;
+
+    REP(j, ANTS)
+      if( ants[shortestAnt].getPath() < ants[j].getPath())
+        shortestAnt = j;
+
+    shortestPathLength = ants[shortestAnt].getPath(shortestPath);
+    G.updateGlobally(shortestPath, shortestPathLength);
   }
-
-  int sum = 0;
-  REP(i, path.size()) sum += G.distance[path[i]][path[(i+1)%G.size]];
-
-  return sum;
 }
 
 int main(){
-  CNGraph G;
-
+  srand(time(0));
   readData(G);
 
   vector<int> path;
